9ce0b230b317122d1c516d3cdafecd113440368f
Conrad-Yeung <conrad.yeung@hotmail.ca>
2021-05-04 12:42:20 -0400

    fixed label for HDFS

--- Parsing/.ipynb_checkpoints/clean_parse_extract-checkpoint.py
+++ Parsing/.ipynb_checkpoints/clean_parse_extract-checkpoint.py
@@ -28,66 +28,19 @@ def clean_logs(data_file,template_file,log_type):
     ###Output: csv file
     
     #Read in the files from parser.py
-    log_clean = pd.read_csv(data_file,converters={'ParameterList': literal_eval})
+    df_raw = pd.read_csv(data_file)
     template = pd.read_csv(template_file)
+    
     
     #Create more clear EventIds: E1, E2
     template["Clean_EventId"] = ["E"+str(i+1) for i in range(0,len(template.EventId))]
     #Store new EventIds into Dictionary
     events = dict(zip(template.EventId,template.Clean_EventId))
     #Rename EventIds to E1,E2 ...
-    log_clean.EventId = log_clean.EventId.map(events)
-    
-####Create column with blk_id extracted from ParameterList if HDFS
-    if log_type == "HDFS":
-        #Parameters
-        warnings.simplefilter(action="ignore", category=SettingWithCopyWarning) #Ignore some warning messages 
-        count = 0
-        
-        #Some functions for extracting block id
-        def extract_blk_id(x):
-        ###
-        ###Check if entry is a blk_id, return True if value is a blk_id 
-        ###
-        ###Input: x, str
-        ###Output: boolean
-            if "blk" in x:
-                return True
-        
-        #Create blk_id column
-        count = 0
-        log_clean["blk_id"] = "" #Initialize new column
-        for params in log_clean.ParameterList:
-            if params == []: #If the list is empty - when the IPLOM parser failed (on 3 occaisions) @ line 1438,1767,1900
-                #Need to go to Content to get blk_id
-                intermediate = [blkid for blkid in log_clean["Content"][count].split() if extract_blk_id(blkid)] 
-                if len(intermediate) == 1:
-                    log_clean["blk_id"][count] = intermediate
-                else: #BLOCK* ask 10.251.126.5 50010 to delete: Only want the blk_id from * not what is deleted
-                     log_clean["blk_id"][count] = [intermediate[0]]
-            else: 
-                log_clean["blk_id"][count] = [blkid for blkid in params if extract_blk_id(blkid)] #Extract parameter entries with blk_id
-            count+=1
+    df_raw.EventId = df_raw.EventId.map(events)
 
-        #Deal with weird cases such as: 
-        #['blk_-7601381921195183756', 'mnt/hadoop/dfs/data/current/subdir48/blk_-7601381921195183756'] = Keep 1 blk_id
-        #or 
-        #[]'/user/root/randtxt2/_temporary/_task_200811101024_0002_m_000299_0/part-00299. blk_4984150784048864430']  or ['replicate blk_-7571492020523929240 to datanode(s) 10.251.122.38']= Extract blk_id
-        count = 0
-        for row in log_clean.blk_id: 
-            if any("/" in sub for sub in row) or any(" blk" in sub for sub in row): #Rows with weird cases similar to above
-                if len(row) == 2:
-                    temp = row[0]
-                    log_clean["blk_id"][count] = [temp]
-                else:
-                    temp = row[0].split(" ")
-                    idx = [i for i, s in enumerate(temp) if 'blk' in s][0]
-                    log_clean["blk_id"][count] = [temp[idx]]
-            count += 1
-            
     #Create new CSV file log_structured_clean.csv
-    log_clean.to_csv("./Parsing/cleanlogs/"+log_type+"_structured_clean.csv",sep=",",index=False)
-    print("./Parsing/cleanlogs/"+log_type+"_structured_clean.csv"+" created.")
+    df_raw.to_csv("./Parsing/cleanlogs/"+log_type+"_structured_clean.csv",sep=",",index=False)    
 
 #2.1 Label Logs 
 def label_log(data_file,label_file,log_type="HDFS"):
@@ -106,6 +59,16 @@ def label_log(data_file,label_file,log_type="HDFS"):
     anomaly_labels = anomaly.set_index("BlockId")["Label"].to_dict()
     log_clean = pd.read_csv(data_file,converters={'ParameterList': literal_eval})
     warnings.simplefilter(action="ignore", category=SettingWithCopyWarning) #Ignore some warning messages 
+    
+    #Some functions for extracting block id
+    def extract_blk_id(x):
+    ###
+    ###Check if entry is a blk_id, return True if value is a blk_id 
+    ###
+    ###Input: x, str
+    ###Output: boolean
+        if "blk" in x:
+            return True
         
     #Individual Entries inside blk_id
     def extract_label(x): 
@@ -127,12 +90,42 @@ def label_log(data_file,label_file,log_type="HDFS"):
             else: #id does not exist in our dictionary (i.e. do not know label)
                 return "Unavailable"
 
+    #Create blk_id column
+    count = 0
+    log_clean["blk_id"] = "" #Initialize new column
+    for params in log_clean.ParameterList:
+        if params == []: #If the list is empty - when the IPLOM parser failed (on 3 occaisions) @ line 1438,1767,1900
+            #Need to go to Content to get blk_id
+            intermediate = [blkid for blkid in log_clean["Content"][count].split() if extract_blk_id(blkid)] 
+            if len(intermediate) == 1:
+                log_clean["blk_id"][count] = intermediate
+            else: #BLOCK* ask 10.251.126.5 50010 to delete: Only want the blk_id from * not what is deleted
+                 log_clean["blk_id"][count] = [intermediate[0]]
+        else: 
+            log_clean["blk_id"][count] = [blkid for blkid in params if extract_blk_id(blkid)] #Extract parameter entries with blk_id
+        count+=1
+
+    #Deal with weird cases such as: 
+    #['blk_-7601381921195183756', 'mnt/hadoop/dfs/data/current/subdir48/blk_-7601381921195183756'] = Keep 1 blk_id
+    #or 
+    #[]'/user/root/randtxt2/_temporary/_task_200811101024_0002_m_000299_0/part-00299. blk_4984150784048864430']  or ['replicate blk_-7571492020523929240 to datanode(s) 10.251.122.38']= Extract blk_id
+    count = 0
+    for row in log_clean.blk_id: 
+        if any("/" in sub for sub in row) or any(" blk" in sub for sub in row): #Rows with weird cases similar to above
+            if len(row) == 2:
+                temp = row[0]
+                log_clean["blk_id"][count] = [temp]
+            else:
+                temp = row[0].split(" ")
+                idx = [i for i, s in enumerate(temp) if 'blk' in s][0]
+                log_clean["blk_id"][count] = [temp[idx]]
+        count += 1
+        
     #Create label column
     log_clean["label"] = [extract_label(blkid) for blkid in log_clean.blk_id] #Initialize new column
 
     #Export to CSV
     log_clean.to_csv("./Parsing/cleanlogs/"+log_type+"_labelled.csv",sep=",",index=False)
-    print("./Parsing/cleanlogs/"+log_type+"_labelled.csv"+" created.")
     
 #3 Parse and Extract
 #3.1 Session window (HDFS only)
